1be63429888ba8b35f50f88480d89029aa9c348b
Jessica Lord <jessica.lord@github.com>
2014-02-04 18:50:42 -0800

    @maxogden rewrote in 1/4 the code

--- makeTable.js
+++ makeTable.js
@@ -3,52 +3,30 @@ var csv = require('binary-csv')
 var concat = require('concat-stream')
 
 module.exports = function makeTable(KEY, callback) {
+  
   var base = 'https://docs.google.com/spreadsheet/pub?hl=en_US&hl=en_US&key=' 
   var query = '&single=true&gid=0&output=csv'
   var URL = base + KEY + query
+    
+  var csvParser = csv({json: true})
   
-  request(URL).pipe(csv({json: true}).pipe(concat(function(data) {
-    console.log(data)
-  }))
-)
-
-  // request.(URL, function (error, response, body) {
-  //   if (error) return callback(err)
-  //   if (!error && response.statusCode == 200) {
-  //     var array = body.split('\n')
-  //     var headers = array.shift()
-  //     makeMdTable(headers, array, callback)
-  //   }
-  // })
-}
-
-function makeMdTable(headers, bodyArray, callback) {  
-  var headerCount = (headers.split(',').length) + 1
-  var comma = /\,/g
-  var tableHeaders = '|' + headers.replace(comma, '|') + '|'
-  var tableBody = addNLandBar(bodyArray)
-  var underHeaders = makeHeaderUnders(headerCount)
-  var nl = '\n'
-  var mdTable = tableHeaders + nl + underHeaders + nl + tableBody
-  return callback("null", mdTable)
-}
-
-function makeHeaderUnders(headerCount) {
-  var underHeaders = ""
-  for (i = 1; i <= headerCount; i++) {
-    if (i === 1) underHeaders += "| ------ |"
-    else underHeaders += " ------ |"
+  request(URL).pipe(csvParser).pipe(concat(rows))
+  
+  function rows(data) {
+    var table = '|'
+    var headers = Object.keys(data[0])
+    var underHeaders = ''
+    headers.map(function(key) {
+      table += key + '|'
+      underHeaders += ' ------ |'
+    })
+    
+    table += '\n|' + underHeaders + '\n'
+    data.map(function(row) {
+      var values = headers.map(function(h) { return row[h] })
+      table += '|' + values.join('|') + '|\n'
+    })
+    console.log(table)
   }
-  return underHeaders
 }
 
-function addNLandBar(array) {
-  var edit = []
-  var comma = /\,/g
-  array.forEach(function (item) {
-    item = '|' + item.replace(comma, '|') + '|'
-    item += "\n"
-    edit.push(item)
-  })
-  return edit.toString().replace(comma, '')
-}
