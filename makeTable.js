93c8aacf30d968dcde637f1af026dda4317435a2
Jessica Lord <jessica.lord@github.com>
2014-05-23 16:46:48 +0200

    add max's module

--- makeTable.js
+++ makeTable.js
@@ -1,19 +1,42 @@
 var request = require('request')
 var csv = require('binary-csv')
 var concat = require('concat-stream')
+var parser = require('google-spreadsheets-key-parser')
+
+
+// returns:
+// { key: '1jHY4wO4b0kuX4rVnJgZGwfQUzoxado52k1hzxdUY-AM',
+//  isNewSheets: true }
 
 module.exports = function makeTable(key, callback) {
-  var base = 'https://docs.google.com/spreadsheet/pub?hl=en_US&hl=en_US&key='
-  var query = '&single=true&gid=0&output=csv'
-  var URL = base + key + query
+  var newBase = 'https://docs.google.com/spreadsheets/d/'
+  var newQuery = '/export?gid=0&format=csv'
+  var oldBase = 'https://docs.google.com/spreadsheet/pub?hl=en_US&hl=en_US&key='
+  var oldQuery = '&single=true&gid=0&output=csv'
+
+  var formattedURL = ''
+
+  var parsed = parser(URL)
+
+  if (parsed.isNewSheets) {
+    formattedURL = newBase + parsed.key + newQuery
+  } else {
+    formattedURL = oldBase + parsed.key + oldQuery
+  }
+
+  console.log('URL', URL)
+// https://docs.google.com/spreadsheets/d/10z-2P8l88hLF35EodIjigs5cyCD7BtmfZS631JMtIUU/export?gid=0&format=csv
+// https://docs.google.com/spreadsheets/d/1jHY4wO4b0kuX4rVnJgZGwfQUzoxado52k1hzxdUY-AM/export?gid=0&format=csv
+  // https://docs.google.com/spreadsheet/d/1jHY4wO4b0kuX4rVnJgZGwfQUzoxado52k1hzxdUY-AM/export?gid=0&format=csv
+// /export?gid=0&format=csv
 
   var csvParser = csv({json: true})
 
-  var req = request(URL)
+  var req = request(formattedURL)
 
   req.on('response', function (response){
     if (response.statusCode.toString() === "404") {
-      return callback(new Error("Spreadsheet not found.\n"
+      return callback(new Error("HI Spreadsheet not found.\n"
         + "Please double check your spreadsheet key is correct."))
     }
     if (response.req._header.match('ServiceLogin?')) {
