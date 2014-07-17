a601327886d71d365b17e74d9ef50026f0897bf9
Jessica Lord <jessica.lord@github.com>
2014-07-16 22:33:34 -0700

    edit

--- tests/index.js
+++ tests/index.js
@@ -3,3 +3,8 @@
 // - new spreadsheet
 // - spreadsheet that has not been published
 // - spreadsheet that has not been shared
+
+// new url
+var newSS = https://docs.google.com/spreadsheets/d/1jHY4wO4b0kuX4rVnJgZGwfQUzoxado52k1hzxdUY-AM/edit#gid=0
+// old url
+var oldSS = https://docs.google.com/spreadsheet/ccc?key=0AuOjlXjUrSbAdE1XRFJkeEJZQ1NSelhILUR0NXdBWUE#gid=0
