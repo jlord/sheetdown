1a2c227042d6caa888f08a6ccc399e7b7e4fdefc
Jessica Lord <jlord@github.com>
2015-07-25 23:22:47 -0700

    JS Standard Style

--- index.js
+++ index.js
@@ -7,21 +7,21 @@ var key = process.argv[2]
 
 var save = false
 
-if (process.argv[3] && process.argv[3].match("--save")) {
+if (process.argv[3] && process.argv[3].match('--save')) {
   save = true
 }
 
 if (key && !save) {
-  makeTable(key, function callback(err, table) {
-    if (err != "null") return console.error(err)
+  makeTable(key, function callback (err, table) {
+    if (err !== null) return console.error(err)
 
     console.log(table)
   })
 }
 
 if (key && save) {
-  makeTable(key, function callback(err, table) {
-    if (err != "null") return console.error(err)
+  makeTable(key, function callback (err, table) {
+    if (err !== null) return console.error(err)
 
     fs.writeFile('table.md', table.toString(), function (err) {
       if (err) return console.error(err)
@@ -31,6 +31,6 @@ if (key && save) {
 }
 
 if (!key) {
-  return console.log("To use: `$ sheetdown SPREADSHEETKEY --save`\n" +
-                     "Or: `$ sheetdown SPREADSHEETKEY | pbcopy")
+  console.log('To use: `$ sheetdown SPREADSHEETKEY --save`\n' +
+    'Or: `$ sheetdown SPREADSHEETKEY | pbcopy')
 }
