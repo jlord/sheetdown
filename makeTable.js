3789800c9ee84e0edee158ea447b6d0026f304ee
Jessica Lord <jlord@github.com>
2015-07-25 23:23:40 -0700

    JS Standard Style

--- makeTable.js
+++ makeTable.js
@@ -31,34 +31,34 @@ module.exports = function makeTable (fullURL, callback) {
 
   var req = request(formattedURL)
 
-  req.on('response', function (response){
-    if (response.statusCode.toString() === "404") {
-      return callback(new Error("HI Spreadsheet not found.\n"
-        + "Please double check your spreadsheet key is correct."))
+  req.on('response', function (response) {
+    if (response.statusCode.toString() === '404') {
+      return callback(new Error('HI Spreadsheet not found.\n' +
+        'Please double check your spreadsheet key is correct.'))
     }
     if (response.req._header.match('ServiceLogin?')) {
-      return callback(new Error("Cannot access spreadsheet.\n"
-        + "Be sure you 'Publish to the Web' and turn the share settings to public on your spreadsheet."))
+      return callback(new Error('Cannot access spreadsheet.\n' +
+        'Be sure you "Publish to the Web" and turn the share settings to public on your spreadsheet.'))
     }
     req.pipe(csvParser).pipe(concat(rows))
   })
 
-  function rows(data) {
+  function rows (data) {
     var table = '|'
     var headers = Object.keys(data[0])
     var underHeaders = ''
-    headers.map(function(key) {
+    headers.map(function (key) {
       table += key + '|'
       underHeaders += ' ------ |'
     })
 
     table += '\n|' + underHeaders + '\n'
-    data.map(function(row) {
-      var values = headers.map(function(h) {
+    data.map(function (row) {
+      var values = headers.map(function (h) {
         return markdownify(row[h])
       })
       table += '|' + values.join('|') + '|\n'
     })
-    return callback("null", table)
+    return callback(null, table)
   }
 }
